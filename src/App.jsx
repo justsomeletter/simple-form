ca630df525ca7970dba0713c82435f244c8eceed
justsomeletter <rahulchauhan2060@gmail.com>
2023-12-03 22:41:06 +0530

    Update App.jsx
    Added the initial Structure of the Form

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,27 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <div>I am inside App</div>
-    </>
+    <div>
+      <div className="form-body">
+        <form action="">
+          <label>
+            Name <input type="text" />
+          </label>{" "}
+          <br />
+          <label>
+            PUC Date <input type="date" />
+          </label>{" "}
+          <br />
+          <label>
+            UID No. <input type="number" />
+          </label>{" "}
+          <br />
+          <label>
+            Insurance No. <input type="number" />
+          </label>
+        </form>
+      </div>
+    </div>
   );
 }
 
