e98987d38997bc120e0ad34aa8a66157db81d178
justsomeletter <rahulchauhan2060@gmail.com>
2023-12-03 23:02:05 +0530

    Added Server
    Basic server code for my application.

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("I am insinde jasndkjsd");
+});
+
+app.listen(port, () => {
+  console.log(`server is up and running on port ${port}`);
+});
